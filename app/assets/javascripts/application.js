d0846677e886395182968da07d32cdee8dfe6d8f
Emerson Jose <emersonjprogramador@gmail.com>
2016-07-09 14:05:15 -0300

    fixeb jquery ready

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,13 @@
 //= require jquery.backstretch.min
 //= require turbolinks
 //= require_tree .
+
+$(document).on('turbolinks:load', function() {
+    
+    /* Background slideshow */
+    $.backstretch([
+      "/assets/img/backgrounds/1.jpg"
+    , "/assets/img/backgrounds/2.jpg"
+    , "/assets/img/backgrounds/3.jpg"
+    ], {duration: 3000, fade: 750});
+});
